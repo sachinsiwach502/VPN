f6081818f9a43f464a98acb2e23f6b2951b60de7
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-02-16 10:44:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Rubik", "sans - serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
